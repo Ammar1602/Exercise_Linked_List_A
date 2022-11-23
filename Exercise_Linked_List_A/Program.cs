8530491753af28c3575566998d8275a4e28548a9
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2022-11-23 15:43:01 +0700

    add public bool

--- Exercise_Linked_List_A/Program.cs
+++ Exercise_Linked_List_A/Program.cs
@@ -9,9 +9,30 @@ namespace Exercise_Linked_List_A
     class Node
     {
         /*creates Node for the circular list*/
-        public int rollnumber;
+        public int rollNumber;
         public string name;
         public Node next;
     }
-    
+    class CircularList
+    {
+        Node LAST;
+
+        public CircularList()
+        {
+            LAST = null;
+        }
+
+        public bool Search(int rollNo, ref Node previous, ref Node current)/*Search for the specified node*/
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current,  current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);/*If the node is found*/
+            }
+            if (rollNo == LAST.rollNumber)/*If the node is present at the end*/
+                return true;
+            else
+                return (false);/*return false if the node is not found*/
+        }
+    }
 }
