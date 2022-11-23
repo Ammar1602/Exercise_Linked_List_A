2dbb8c0655dcd9e31603e41fbd2a4943fb4d78ce
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2022-11-23 16:27:52 +0700

    add no and name for student

--- Exercise_Linked_List_A/Program.cs
+++ Exercise_Linked_List_A/Program.cs
@@ -77,7 +77,9 @@ namespace Exercise_Linked_List_A
                     Console.WriteLine("1. View all the records in the list");
                     Console.WriteLine("2. Search for a record in the list");
                     Console.WriteLine("3. Display the first record in the list");
-                    Console.WriteLine("4. Exit");
+                    Console.WriteLine("4. Nomor Siswa");
+                    Console.WriteLine("5. Nama Siswa");
+                    Console.WriteLine("6. Exit");
                     Console.WriteLine("\nEnter your choice (1-4): ");
                     char ch = Convert.ToChar(Console.ReadLine());
                     switch (ch)
