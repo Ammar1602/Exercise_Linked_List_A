69a4de60ca8d0b2bf88744b6465c816bd469973e
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2022-11-23 15:32:22 +0700

    Add project files.

---
+++ Exercise_Linked_List_A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Linked_List_A
+{
+    class Node
+    {
+        /*creates Node for the circular list*/
+        public int rollnumber;
+        public string name;
+        public Node next;
+    }
+    
+}
